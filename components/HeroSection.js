e72c269fc99b7c4f32ae5bdcdc7b85f836034a1b
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-09-05 21:21:30 -0700

    Fix date and venue

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,7 +6,7 @@ export function HeroSection(props){
         <div className="delimitationpurple  xl:px-[140px]  w-full h-[700px] bg-[hsla(36,76%,91%,36%)]">
             <div className="delimitationred  md:ml-[10%] mt-[10%] lg:w-[50%] md:w-[70%] w-[90%] m-auto lg:p-[50px] md:p-[40px] md:py-0 py-[6%] p-[2%] bg-[hsla(35,83%,93%,72%)] rounded-2xl">
                 <h1 className=" delimitationgreen font-bold mb-[10px] text-[hsl(67,23%,55%)] md:text-[57px] text-[50px] ">CAUSE HACKS</h1>
-                <h3 className="font-bold mb-[30px] text-[hsl(11,39%,57%)] ">October 10th, San Diego Coliseum</h3>
+                <h3 className="font-bold mb-[30px] text-[hsl(11,39%,57%)] ">October 13th, VentureX San Diego</h3>
                 <p className=" mb-[30px] text-[hsl(0,0%,0%)] text-[20px] font-bold leading-8">An unparalleled chance to unite with ambitious students and progressive organizations, 
                     creating solutions for pressing social issues.</p>
                 <button className=" w-[110px] h-[50px]  flex justify-center items-center font-bold  text-[hsl(0,0%,100%)] bg-[hsl(11,39%,57%)] rounded-xl">Register</button>
